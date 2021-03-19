a5c978dfc98e3c54ebf3344f4082b8b777d71d61
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-19 15:59:59 -0700

    basic backend setup. Next is adding models

---
+++ models/index.js
@@ -0,0 +1,11 @@
+// const User = require("./User");
+// const Article = require("./Article");
+// const Comment = require("./Comment");
+// const Source = require("./Source");
+// const Image = require("./Image");
+// const Edit = require("./Edit");
+// const Vote = require("./Vote");
+
+// TODO: define model relationships here
+
+// module.exports = { User, Article, Comment, Source, Image, Edit, Vote };
