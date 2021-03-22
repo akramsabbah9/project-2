bb4b20613a216ec003345c5c49a0456146d298af
Nina Jade <jaderiver64@gmail.com>
2021-03-21 23:47:00 -0500

    testing revision-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const router = require("express").Router();
 const articleRoutes = require("./article-routes");
 const commentRoutes = require("./comment-routes");
 const userRoutes = require("./user-routes");
+const revisionRoutes = require("./revision-routes");
 
 
 router.use("/articles", articleRoutes);
 router.use("/comments", commentRoutes);
 router.use("/users", userRoutes);
+router.use("/revisions", revisionRoutes);
 
 module.exports = router;
