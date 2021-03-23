f0b6cfdb1d3a8a0fbb4996db5c0e5345cbfc2e07
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-23 12:53:37 -0700

    added home-routes.js

---
+++ controllers/home-routes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Article } = require("../models");
+
+// front page: get article count
+
+// single article: render a single article
+
+module.exports = router;
