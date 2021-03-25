97a3229b38283d76884ab01f9b8033f21e0ea5fa
Nina Jade <jaderiver64@gmail.com>
2021-03-25 09:18:22 -0500

    adding helpers and withAuth

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// ensures the user is logged in before proceeding 
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
