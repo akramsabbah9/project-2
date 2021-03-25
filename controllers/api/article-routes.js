cae36ba0f220d05f955803664a9c2b32557d145d
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-25 02:31:38 -0700

    post a new revision whenever an article is created or updated

--- controllers/api/article-routes.js
+++ controllers/api/article-routes.js
@@ -96,25 +96,38 @@ router.get("/:id", (req, res) => {
 
 
 // post a new article
-// TODO: maybe add the newly-created article to the revision table when it is made?
 router.post("/", (req, res) => {
     // expects { title, content } in req.body
-    Article.create({
-        title: req.body.title,
-        content: req.body.content
-    })
-    .then(articleData => res.json(articleData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    if (req.session.user_id) { //TODO: remove once withAuth is added
+        Article.create({
+            title: req.body.title,
+            content: req.body.content
+        })
+        .then(articleData => {
+            // grab id from response data
+            const article = articleData.get({ plain: true });
+            const article_id = article.id;
+
+            // if create successful, make a revision with this information
+            Revision.create({
+                changes: req.body.content,
+                user_id: req.session.user_id,
+                article_id: article_id
+            })
+            .then(revisionData => res.json(revisionData));
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 });
 
 
 // (put) vote on an article by id
 router.put("/vote", checkVote, (req, res) => {
     // expects { value, user_id, article_id } in req.body
-    if (req.session) {
+    if (req.session.user_id) { //TODO: remove once withAuth is added
         Article.vote(
             { ...req.body, user_id: req.session.user_id },
             {Vote, Comment, User, Image, Revision }
@@ -129,27 +142,34 @@ router.put("/vote", checkVote, (req, res) => {
 
 
 // (put) update an article by id
-// TODO: maybe interface with the revision table, as done in votes?
 router.put("/:id", (req, res) => {
     // expects { title, content } in req.body
-    Article.update(
-    {
-        title: req.body.title,
-        content: req.body.content
-    },
-    {
-        where: { id: req.params.id }
-    })
-    .then(articleData => {
-        if (!articleData[0]) {
-            return res.status(404).json({ message: "No article found with this id" });
-        }
-        res.json(articleData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    if (req.session.user_id) { //TODO: remove once withAuth is added
+        Article.update(
+        {
+            title: req.body.title,
+            content: req.body.content
+        },
+        {
+            where: { id: req.params.id }
+        })
+        .then(articleData => {
+            if (!articleData[0]) {
+                return res.status(404).json({ message: "No article found with this id" });
+            }
+            // if update successful, make a revision with these changes
+            Revision.create({
+                changes: req.body.content,
+                user_id: req.session.user_id,
+                article_id: req.params.id
+            })
+            .then(revisionData => res.json(revisionData));
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 });
 
 
