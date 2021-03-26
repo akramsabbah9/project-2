ee106e9501b5952aa3da253ebf0c8e3cd780cab0
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-25 17:45:03 -0700

    refactored revision history script

--- public/javascript/history.js
+++ public/javascript/history.js
@@ -24,23 +24,4 @@ const revisionButtonHandler = async event => {
     }
 };
 
-// update an article with a past revision's content
-const revert = async(article_id, content) => {
-    const response = await fetch(`/api/articles/${article_id}`, {
-        method: "PUT",
-        body: JSON.stringify({
-            content: content
-        }),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-
-    if (response.ok) {
-        window.location.href = `/article/${article_id}`;
-    } else {
-        alert(response.statusText);
-    }
-};
-
 document.querySelector("#revision-history").addEventListener("click", revisionButtonHandler);
