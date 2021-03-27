60fa3f844a9f09ec8598647c8846059730b49c69
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-27 11:42:39 -0700

    you can now add images when creating articles

--- public/javascript/add-article.js
+++ public/javascript/add-article.js
@@ -3,6 +3,17 @@ async function newFormHandler(event) {
 
     const title = document.querySelector('input[name="article-title"]').value;
     const article_content = document.querySelector('textarea[name="article-content"]').value;
+    const imgUrl = document.querySelector("input[name='img-url']").value;
+
+    // error checking: if title/content is null, stop the user from continuing.
+    if (!title) {
+        alert("You cannot leave the title of this article blank!");
+        return;
+    }
+    if (!article_content) {
+        alert("You cannot leave the content of this article blank!");
+        return;
+    }
 
     const response = await fetch(`/api/articles`, {
         method: 'POST',
@@ -18,9 +29,27 @@ async function newFormHandler(event) {
     if(response.ok) {
         const newArticle = await response.json();
         const newArticleId = newArticle.article_id;
+
+        if (imgUrl) {
+            const imagefetch = await fetch(`/api/images`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    image_url: imgUrl,
+                    article_id: newArticleId
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (!imagefetch.ok) {
+                alert("The image you used could not be added.");
+                alert("response.statusT")
+            }
+        }
         
         document.location.replace(`/article/${newArticleId}`);
-    }else {
+    }
+    else {
         alert(response.statusText);
     }
 }
