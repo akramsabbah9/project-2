fc96f48104a61d13b30d1470b8b53a092c055583
Nina Jade <jaderiver64@gmail.com>
2021-03-28 02:11:32 -0500

    added event listener for search bar and fetch code for searches.  Not yet displaying results in the console.  More work needed

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,69 +1,86 @@
-const lunr = require("lunr");
+async function searchFormHandler(event) {
+    event.preventDefault();
 
-const articles = [
-    /*
-    // Tester Code
-    {
-        "id": "1",
-        "title": "Nuggles is the Queen",
-        "content": "Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff",
-        "created_at": "2021-03-23T03:54:01.000Z",
-        "updated_at": "2021-03-23T03:54:01.000Z",
-        "vote_count": null,
-        "comments": [{
-                "id": 1,
-                "comment_text": "smeg",
-                "user_id": 1,
-                "article_id": 1,
-                "created_at": "2021-03-23T03:54:20.000Z",
-                "user": {
-                    "username": "arnyRimmer"
-                }
-            },
-            {
-                "id": 2,
-                "comment_text": "smeg",
-                "user_id": 2,
-                "article_id": 1,
-                "created_at": "2021-03-23T04:08:46.000Z",
-                "user": {
-                    "username": "nuggles"
-                }
-            },
-            {
-                "id": 3,
-                "comment_text": "smeg",
-                "user_id": 2,
-                "article_id": 1,
-                "created_at": "2021-03-23T04:08:47.000Z",
-                "user": {
-                    "username": "nuggles"
-                }
+    const searchTerm = document.querySelector('input[name="search-bar"]').value;
+
+    console.log(content);
+
+    const response = await fetch(`/api/articles/`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const lunr = require("lunr");
+
+    if (response.ok) {
+        const articles = await response.json();
+        const idx = lunr(function() {
+            this.field("title");
+            this.field("content");
+            for (let i = 0; i < articles.length; i++) {
+                this.add(articles[i])
             }
-        ],
-    }*/
-]
+        });
 
+        function searchArticles(query) {
+            const output = idx.search(query)
 
+            return output.map(item => {
+                return articles.find(article => item.ref === article.id)
+            })
+        }
+        const result = searchArticles(searchTerm);
+        console.log(result);
 
-const idx = lunr(function() {
-    this.field("title");
-    this.field("content");
-
-
-
-    for (let i = 0; i < articles.length; i++) {
-        this.add(articles[i])
+    } else {
+        alert(response.statusText);
     }
-});
-
-function searchArticles(query) {
-    const output = idx.search(query)
-
-    return output.map(item => {
-        return articles.find(article => item.ref === article.id)
-    })
 }
 
-const result = searchArticles('Nuggles')
-console.log(result);
+
+
+document.querySelector('#search-form').addEventListener('submit', searchFormHandler);
+
+
+/*
+   // Tester Code
+   {
+       "id": "1",
+       "title": "Nuggles is the Queen",
+       "content": "Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff",
+       "created_at": "2021-03-23T03:54:01.000Z",
+       "updated_at": "2021-03-23T03:54:01.000Z",
+       "vote_count": null,
+       "comments": [{
+               "id": 1,
+               "comment_text": "smeg",
+               "user_id": 1,
+               "article_id": 1,
+               "created_at": "2021-03-23T03:54:20.000Z",
+               "user": {
+                   "username": "arnyRimmer"
+               }
+           },
+           {
+               "id": 2,
+               "comment_text": "smeg",
+               "user_id": 2,
+               "article_id": 1,
+               "created_at": "2021-03-23T04:08:46.000Z",
+               "user": {
+                   "username": "nuggles"
+               }
+           },
+           {
+               "id": 3,
+               "comment_text": "smeg",
+               "user_id": 2,
+               "article_id": 1,
+               "created_at": "2021-03-23T04:08:47.000Z",
+               "user": {
+                   "username": "nuggles"
+               }
+           }
+       ],
+   }*/
