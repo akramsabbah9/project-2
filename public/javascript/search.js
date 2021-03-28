f5a91890d88bf595baddc28ab4321c8f266527c7
Nina Jade <jaderiver64@gmail.com>
2021-03-28 12:39:38 -0500

    Buggy Search Code!

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,3 +1,46 @@
+let searchTerm = document.querySelector('input[name="search-bar"]').value;
+searchTerm = `*${searchTerm}*`;
+
+
+async function searchFormHandler(event) {
+    event.preventDefault();
+    const response = await fetch(`/api/articles/`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        const articles = await response.json();
+        const idx = lunr(function() {
+            this.field("title");
+            this.field("content");
+
+            for (let i = 0; i < articles.length; i++) {
+                this.add(articles[i])
+            }
+        });
+
+
+        function searchArticles(index, query, articlesArray) {
+            const output = index.search(query);
+            console.log(output);
+            let ret = articlesArray.filter(article => output.find(item => parseInt(item.ref) === article.id));
+            return ret;
+        }
+
+        const result = searchArticles(idx, searchTerm, articles)
+        console.log(result);
+        console.log(articles);
+    } else {
+        alert(response.statusText);
+    }
+}
+
+
+
+document.querySelector('#search-form').addEventListener('submit', searchFormHandler);
+
 const articleTest = [{
     "id": "1",
     "title": "Nuggles is the Queen",
@@ -37,86 +80,3 @@ const articleTest = [{
         }
     ]
 }]
-
-async function searchFormHandler(event) {
-    event.preventDefault();
-    const searchTerm = document.querySelector('input[name="search-bar"]').value;
-    const response = await fetch(`/api/articles/`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    if (response.ok) {
-        const articles = await response.json();
-        const idx = lunr(function() {
-            this.field("title");
-            this.field("description");
-
-            for (let i = 0; i < articles.length; i++) {
-                this.add(articles[i])
-            }
-        });
-
-
-        function searchArticles(query) {
-            const output = idx.search(query)
-
-            return output.map(item => {
-                return articles.find(article => item.ref === article.id)
-            })
-        }
-
-        const result = searchArticles(searchTerm)
-        console.log(result);
-        console.log(articles);
-
-    } else {
-        alert(response.statusText);
-    }
-}
-
-document.querySelector('#search-form').addEventListener('submit', searchFormHandler);
-
-
-/*
-   // Tester Code
-   [{
-       "id": "1",
-       "title": "Nuggles is the Queen",
-       "content": "Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff, Stuff",
-       "created_at": "2021-03-23T03:54:01.000Z",
-       "updated_at": "2021-03-23T03:54:01.000Z",
-       "vote_count": null,
-       "comments": [{
-               "id": 1,
-               "comment_text": "smeg",
-               "user_id": 1,
-               "article_id": 1,
-               "created_at": "2021-03-23T03:54:20.000Z",
-               "user": {
-                   "username": "arnyRimmer"
-               }
-           },
-           {
-               "id": 2,
-               "comment_text": "smeg",
-               "user_id": 2,
-               "article_id": 1,
-               "created_at": "2021-03-23T04:08:46.000Z",
-               "user": {
-                   "username": "nuggles"
-               }
-           },
-           {
-               "id": 3,
-               "comment_text": "smeg",
-               "user_id": 2,
-               "article_id": 1,
-               "created_at": "2021-03-23T04:08:47.000Z",
-               "user": {
-                   "username": "nuggles"
-               }
-           }
-       ],
-   }*/
